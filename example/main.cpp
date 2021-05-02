f0b09c24a086cef5ac1238580924f08ceb8999f0
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-05-02 21:58:45 +0200

    Clean example file

--- example/main.cpp
+++ example/main.cpp
@@ -1,10 +1,3 @@
-/*#include <boost/iostreams/copy.hpp>
-#include <boost/iostreams/filter/gzip.hpp>
-#include <boost/iostreams/filtering_streambuf.hpp>
-#include <fstream>
-#include <iostream>
-#include <sstream>*/
-
 #include "bio/fasta.hpp"
 #include <exception>
 #include <fstream>
@@ -18,15 +11,4 @@ int main(int argc, char **argv) {
     ss << file.rdbuf();
 
     Fasta::Fasta test = Fasta::read(ss);
-
-    /*std::ifstream file("hello.gz", std::ios_base::in | std::ios_base::binary);
-    boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
-    in.push(boost::iostreams::gzip_decompressor());
-    in.push(file);
-
-    std::stringstream test;
-
-    boost::iostreams::copy(in, test);
-
-    std::cout << test.str();*/
 }
