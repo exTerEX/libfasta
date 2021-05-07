2a133e03b50d72c9820a9689b371a90502db1c22
exTerEX <developer@sagen.io>
2021-05-07 08:53:46 +0200

    Rewritten header file. New parameter names, new methods, etc.

--- include/bio/fasta.hpp
+++ include/bio/fasta.hpp
@@ -15,51 +15,49 @@
 
 namespace Fasta {
 
-class Block {
-  private:
-    std::string Header;
-    std::string Comment;
-    std::string Sequence;
+typedef struct block {
+    std::string header;
+    std::string comment;
+    std::string sequence;
 
-  protected:
-  public:
-    Block() = default;
-    Block(std::string, std::string, std::string = NULL);
-    ~Block() = default;
+    block() = default;
+    block(std::string header, std::string sequence);
+    block(std::string header, std::string comment, std::string sequence);
+    ~block() = default;
 
-    std::string getHeader() const;
-    std::string getComment() const;
-    std::string getSequence() const;
-};
+    void erase();
+} block_t;
 
 class Fasta {
   private:
-    std::vector<Block> Blocks;
+    std::vector<block_t> Blocks;
 
   protected:
-    std::vector<Block> getBlocks() const;
+    std::vector<block_t> getBlocks() const;
 
   public:
     Fasta() = default;
-    Fasta(Block);
-    Fasta(std::vector<Block>);
+    Fasta(block_t __x);
+    Fasta(std::vector<block_t> __x);
     ~Fasta() = default;
 
-    Block operator[](unsigned) const;
-    Block at(unsigned) const;
+    block_t operator[](unsigned index) const;
+    block_t at(unsigned index) const;
 
-    Fasta &operator+(const Fasta &);
-    Fasta &operator+=(const Fasta &);
+    Fasta &operator+(const Fasta &rhs);
+    Fasta &operator+=(const Fasta &rhs);
 
-    bool operator==(Fasta *) const;
-    bool operator!=(Fasta *) const;
+    bool operator==(Fasta *rhs) const;
+    bool operator!=(Fasta *rhs) const;
 
     bool empty() const;
 
-    void push_back(Block);
+    void push_back(const block_t &__x);
 
-    std::vector<Block>::iterator begin();
-    std::vector<Block>::iterator end();
+    std::size_t size() const;
+
+    std::vector<block_t>::iterator begin();
+    std::vector<block_t>::iterator end();
 };
 
 Fasta read(std::stringstream &);
