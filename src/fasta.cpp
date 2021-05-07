a678dd1890b4bdb838c59eefea0cc7158c882c6d
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-05-07 08:56:10 +0200

    Moved read/write methods to own file, reflected changes to block interface and general API interface.

--- src/fasta.cpp
+++ src/fasta.cpp
@@ -8,19 +8,18 @@
  */
 
 #include "bio/fasta.hpp"
-#include <sstream>
 
 namespace Fasta {
 
-Fasta::Fasta(Block block) {}
+Fasta::Fasta(block_t __x) { this->Blocks.push_back(__x); }
 
-Fasta::Fasta(std::vector<Block> blocks) { this->Blocks = blocks; }
+Fasta::Fasta(std::vector<block_t> __x) { this->Blocks = __x; }
 
-std::vector<Block> Fasta::getBlocks() const { return this->Blocks; }
+std::vector<block_t> Fasta::getBlocks() const { return this->Blocks; }
 
-Block Fasta::operator[](unsigned index) const { return this->at(index); }
+block_t Fasta::operator[](unsigned index) const { return this->at(index); }
 
-Block Fasta::at(unsigned index) const { return this->Blocks[index]; }
+block_t Fasta::at(unsigned index) const { return this->Blocks[index]; }
 
 Fasta &Fasta::operator+(const Fasta &rhs) {
     for (auto &element : rhs.getBlocks()) {
@@ -38,15 +37,15 @@ bool Fasta::operator==(Fasta *rhs) const {
     }
 
     for (size_t index = 0; index < this->Blocks.size(); index++) {
-        if (this->Blocks[index].getHeader() != rhs->Blocks[index].getHeader()) {
+        if (this->Blocks[index].header != rhs->Blocks[index].header) {
             return false;
         }
 
-        if (this->Blocks[index].getComment() != rhs->Blocks[index].getComment()) {
+        if (this->Blocks[index].comment != rhs->Blocks[index].comment) {
             return false;
         }
 
-        if (this->Blocks[index].getSequence() != rhs->Blocks[index].getSequence()) {
+        if (this->Blocks[index].sequence != rhs->Blocks[index].sequence) {
             return false;
         }
     }
@@ -59,15 +58,15 @@ bool Fasta::operator!=(Fasta *rhs) const {
     }
 
     for (size_t index = 0; index < this->Blocks.size(); index++) {
-        if (this->Blocks[index].getHeader() != rhs->Blocks[index].getHeader()) {
+        if (this->Blocks[index].header != rhs->Blocks[index].header) {
             return true;
         }
 
-        if (this->Blocks[index].getComment() != rhs->Blocks[index].getComment()) {
+        if (this->Blocks[index].comment != rhs->Blocks[index].comment) {
             return true;
         }
 
-        if (this->Blocks[index].getSequence() != rhs->Blocks[index].getSequence()) {
+        if (this->Blocks[index].sequence != rhs->Blocks[index].sequence) {
             return true;
         }
     }
@@ -76,69 +75,12 @@ bool Fasta::operator!=(Fasta *rhs) const {
 
 bool Fasta::empty() const { return this->Blocks.empty(); }
 
-void Fasta::push_back(Block block) { this->Blocks.push_back(block); }
+void Fasta::push_back(const block_t &__x) { this->Blocks.push_back(__x); }
 
-std::vector<Block>::iterator Fasta::begin() { return this->Blocks.begin(); }
+std::size_t Fasta::size() const { return this->Blocks.size(); }
 
-std::vector<Block>::iterator Fasta::end() { return this->Blocks.end(); }
+std::vector<block_t>::iterator Fasta::begin() { return this->Blocks.begin(); }
 
-Fasta read(std::stringstream &ss) {
-    std::string currentHeader;
-    std::string currentComment;
-    std::string currentSequence;
-
-    // Output object
-    Fasta Output;
-
-    // Switch value to ensure comment treated as comment
-    bool sequenceReached = true;
-
-    // Read lines in stringstream
-    for (std::string line; std::getline(ss, line);) {
-        if ((line[0] == '>' || line[0] == ';') && sequenceReached) {
-            // Shrink sequence size
-            currentSequence.shrink_to_fit();
-
-            // Flush previous block
-            if (currentHeader.empty()) {
-                Output.push_back(Block{currentHeader, currentSequence, currentComment});
-            }
-
-            // Reset temp values
-            currentHeader.clear();
-            currentComment.clear();
-            currentSequence.clear();
-
-            // Remove '>' in header line
-            line.erase(0, 1);
-
-            // Add current line to current header
-            currentHeader = line;
-            currentHeader.shrink_to_fit();
-
-            // Set sequenceReached flag
-            sequenceReached = false;
-        } else if (line[0] == ';' && !sequenceReached) {
-            // Remove ';' in comment line
-            line.erase(0, 1);
-
-            // Add current line to current comment
-            currentComment += line;
-        } else {
-            // Shrink comment size
-            currentComment.shrink_to_fit();
-
-            // Add current line to current sequence
-            currentSequence += line;
-
-            // Set sequenceReached flag
-            sequenceReached = true;
-        }
-    }
-
-    return Output;
-}
-
-std::stringstream write(Fasta Content) { return std::stringstream(NULL); }
+std::vector<block_t>::iterator Fasta::end() { return this->Blocks.end(); }
 
 } // namespace Fasta
