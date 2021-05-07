db095d17178eadc45f900041a36b5215655e5ca6
exTerEX <developer@sagen.io>
2021-05-07 14:21:48 +0200

    Changed CMakeLists to changes done in src and added write method to its own source file

---
+++ src/write.cpp
@@ -0,0 +1,16 @@
+/**
+ * \file write.cpp
+ *
+ * \brief Source code for file/stream write methods for FASTA files and objects.
+ *
+ * \copyright Copyright (c) 2021 Andreas Sagen
+ */
+
+#include "bio/fasta.hpp"
+#include <sstream>
+
+namespace Fasta {
+
+std::stringstream write(Fasta Content) { return std::stringstream(NULL); }
+
+} // namespace Fasta
