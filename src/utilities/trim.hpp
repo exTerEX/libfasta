a815d9967654fd8e304e011a1050a06e95ac811f
exTerEX <developer@sagen.io>
2021-05-12 02:42:43 +0200

    Added trim methods to solve leading/trailing whitespace issues

---
+++ src/utilities/trim.hpp
@@ -0,0 +1,17 @@
+/**
+ * \file trim.hpp
+ *
+ * \brief Header file for sequence/string trim methods.
+ *
+ * \copyright Copyright (c) 2021 Andreas Sagen
+ */
+
+#include <string>
+
+#ifndef TRIM_HPP
+#define TRIM_HPP
+
+std::string &ltrim(std::string &__s);
+std::string &rtrim(std::string &__s);
+
+#endif // TRIM_HPP
